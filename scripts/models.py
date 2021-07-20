965a4dd98af4cf3aa5ff400d1c5787b4cceb4bb4
Quvotha <nndropout1+github@gmail.com>
2021-07-21 02:35:21 +0900

    Add `initialize_model()`

--- scripts/models.py
+++ scripts/models.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 from typing import Tuple
 
+import torch.nn as nn
+from torchvision import models
+
 from dataset import IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_NUM_CHANNELS
 
 
@@ -88,3 +91,78 @@ class AutoEncoderV01(nn.Module):
         encoded = self.encode(batch_flattened)
         decoded = self.decode(encoded)
         return batch_flattened, encoded, decoded
+
+
+def initialize_model(model_name: str, num_classes: int = 1) -> tuple:
+    """Get model architecture.
+
+    Parameters
+    ----------
+    model_name : str
+        Should be one of 'resnet', 'alexnet', 'vgg', 'squeezenet', 'densenet', 'inception'.
+    num_classes : int, optional
+        [description], by default 1
+
+    Returns
+    -------
+    (model, input_size): Tuple
+        Model object and input image size.
+
+    Refference
+    ----------
+    https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+    """
+    model_ft = None
+    input_size = 0
+
+    if model_name == 'resnet':
+        # Resnet18
+        model_ft = models.resnet18(pretrained=False)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == 'alexnet':
+        # Alexnet
+        model_ft = models.alexnet(pretrained=False)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == 'vgg':
+        # VGG11_bn
+        model_ft = models.vgg11_bn(pretrained=False)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == 'squeezenet':
+        # Squeezenet
+        model_ft = models.squeezenet1_0(pretrained=False)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == 'densenet':
+        # Densenet
+        model_ft = models.densenet121(pretrained=False)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == 'inception':
+        # Inception v3
+        # Be careful, expects (299,299) sized images and has auxiliary output
+        model_ft = models.inception_v3(pretrained=False)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 299
+
+    else:
+        raise ValueError(f'{model_name} is not valid model name')
+
+    return model_ft, input_size
